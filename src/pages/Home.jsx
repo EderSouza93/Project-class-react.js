c319b956a651f815cd9c2cb3c35aae65bc2ae1d3
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2025-01-15 13:40:22 -0300

    :sparkles: feat: Header Component - Class-04

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import Header from "../components/Header/Header"
+
 const Home = () => {
 
     return (
         <>
+            <Header/>
             <h1>Home</h1>
             <p>Subtitle</p>
         </>
