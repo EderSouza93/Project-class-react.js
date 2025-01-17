7a98fb6838987761946faca27ff074acf60329ab
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2025-01-17 14:30:36 -0300

    :sparkles: feat: Footer component - Class-06

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,10 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // ASSETS 
 import './Footer.css'
 import Logo from '../../assets/dnc-logo.svg'
-// import BrazilIcon from '../../assets/brazil-icon.svg'
-// import UsaIcon from '../../assets/use-icon.svg'
+import BrazilIcon from '../../assets/brazil-icon.svg'
+import UsaIcon from '../../assets/usa-icon.svg'
 import FacebookIcon from '../../assets/facebook-icon.svg'
 import LinkedinIcon from '../../assets/linkedin-icon.svg'
 import InstagramIcon from '../../assets/instagram-icon.svg'
@@ -16,31 +16,48 @@ const Footer = () => {
             <div className="container">
                 <div className="d-flex jc-space-between">
                     <div className="footer-logo-col">
-                    <img src={Logo} />
-                    <p className="grey-1-color">A escola que prepara você para as profissões em alta no mercado de trabalho.</p>
-                    <div className="d-flex soscial-links">
-                        <a href="https:www.google.com.br" target="_blank">
-                            <img src={FacebookIcon}/>
-                        </a>
-                        <a href="https:www.google.com.br" target="_blank">
-                            <img src={TwitterIcon}/>
-                        </a>
-                        <a href="https:www.google.com.br" target="_blank">
-                            <img src={LinkedinIcon}/>
-                        </a>
-                        <a href="https:www.google.com.br" target="_blank">
-                            <img src={FacebookIcon}/>
-                        </a>
-                        <a href="https:www.google.com.br" target="_blank">
-                            <img src={InstagramIcon}/>
-                        </a>
+                        <img src={Logo} className="footer-logo"/>
+                        <p className="grey-1-color">A escola que prepara você para as profissões em alta no mercado de trabalho.</p>
+                        <div className="d-flex social-links">
+                            <a href="https:www.google.com.br" target="_blank">
+                                <img src={FacebookIcon}/>
+                            </a>
+                            <a href="https:www.google.com.br" target="_blank">
+                                <img src={TwitterIcon}/>
+                            </a>
+                            <a href="https:www.google.com.br" target="_blank">
+                                <img src={LinkedinIcon}/>
+                            </a>
+                            <a href="https:www.google.com.br" target="_blank">
+                                <img src={InstagramIcon}/>
+                            </a>
+                        </div>
                     </div>
-
+                    <div className="d-flex">
+                        <div className='footer-col'>
+                            <h3>Pages</h3>
+                            <ul>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/about">About</Link></li>
+                                <li><Link to="/projects">Projects</Link></li>
+                                <li><Link to="/contact">Contact</Link></li>
+                            </ul>
+                        </div>
+                        <div className="footer-col">
+                            <h3>Contact</h3>
+                            <p className="gray-1-color">R. Justino Cobra, 61 – Vila Ema | São José dos Campos – SP | CEP 12243-030</p>
+                            <p className="gray-1-color">suporte@escoladnc.com.br</p>
+                            <p className="gray-1-color">(19) 99187-4342</p>
+                        </div>
                     </div>
-
-                    
                 </div>
-                
+                <div className="d-flex jc-space-between footer-copy">
+                    <p className="grey-1-color">Copyright © DNC - 2024</p>
+                    <div className="langs-area d-flex">
+                        <img src={BrazilIcon} height={29}/>
+                        <img src={UsaIcon} height={29}/>
+                    </div>
+                </div>    
             </div>
         </footer>
     )
