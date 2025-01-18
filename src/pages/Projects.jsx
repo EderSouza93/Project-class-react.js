04b5f2a55b3d2c8fa8346f9773561a5a85735b1e
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2025-01-17 23:11:52 -0300

    :sparkles: feat: Components in hero and Projecs - class-11

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,19 @@
+import Banner from "../components/Banner/Banner"
+import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer"
+import ProjectsList from "../components/ProjectsList/ProjectsList"
+
 const Projects = () => {
 
     return (
         <>
-            <h1>Projects</h1>
-            <p>Subtitle projects</p>
+            <Header/>
+            <Banner title="projects" image="projects.jpg"/>
+            <div className="container">
+                
+                <ProjectsList/>
+            </div>
+            <Footer/>
         </>
     )
 }
