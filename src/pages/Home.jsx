a44158bcf36c30a12058b6aa4c9115e9a1f82024
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2025-01-17 22:10:44 -0300

    :sparkles: feat: Hero Component - Class-09

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Button from "../components/Button/Button"
-import Banner from "../components/Banner/Banner"
 import Header from "../components/Header/Header"
+import Hero from "../components/Hero/Hero"
 import Footer from "../components/Footer/Footer"
 
 const Home = () => {
@@ -11,7 +11,9 @@ const Home = () => {
             <h1>Home</h1>
             <p>Subtitle</p>
             <Button buttonStyle="secondary" arrow >Olá!</Button>
-            <Banner title="Home" image="about.jpg" />
+            <div className="container">
+                <Hero/>
+            </div>
             <Footer/>
         </>
     )
