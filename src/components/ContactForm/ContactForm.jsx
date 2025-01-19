45d4ad5ea5bf16962844d782a2bcec3359a1de79
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2025-01-19 11:24:02 -0300

    :sparkles: feat: form post e-mailfirst class

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,7 +5,41 @@ import './ContactForm.css'
 import Button from '../Button/Button'
 
 const ContactForm = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+    const [isFormValid, setIsFormValid] = useState(false)
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (isFormValid) {
+            null
+        }
+    }
+
+    useEffect(() => {
+        const isValidEmail = (email) => {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+            return emailRegex.test(email)
+        }
+
+        const isValid = formData.name.trim() && 
+        formData.email.trim() && 
+        isValidEmail(formData.email) && 
+        formData.message.trim()
+
+        setIsFormValid(isValid)
+    }, [formData])
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
     return (
         <div className='contact-form d-flex fd-column al-center'>
             <h2>We love meeting new people and helping them.</h2>
@@ -17,6 +51,7 @@ const ContactForm = () => {
                         id='name'
                         name='name'
                         placeholder='Name *' 
+                        onChange={handleChange}
                     />
                     <input 
                         className='form-input'
@@ -24,6 +59,7 @@ const ContactForm = () => {
                         id='email'
                         name='email'
                         placeholder='Email *' 
+                        onChange={handleChange}
                     />
                     
                 </div>
@@ -33,12 +69,13 @@ const ContactForm = () => {
                         id='message'
                         name='message'
                         placeholder='Message *'
+                        onChange={handleChange}
                         rows='4'
                     ></textarea>
                 </div>
                 <div className='al-center d-flex jc-end form-group'>
                     <div>
-                        <Button type="submit" buttonStyle="secondary">
+                        <Button type="submit" buttonStyle="secondary" disabled={!isFormValid}>
                             Enviar
                         </Button>
                     </div>
